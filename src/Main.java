fe73d6ed80bd0fc22d8cd8f4afa477bbcd9312b5
Lab_04_Part1_Finished <gavin.heid04@gmail.com>
2024-02-11 02:01:39 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int birthMonth = 45;//simulated input
+
+        if (birthMonth >=1 && birthMonth <=12)
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else
+        {
+            System.out.println("You entered an incorrect month value " + birthMonth);
+        }
+    }
+}
