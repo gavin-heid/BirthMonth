06bea017fe045ba50de2b7ab05745b33fe7f42b7
Lab_04_Part1_Finished <gavin.heid04@gmail.com>
2024-02-11 21:11:32 -0500

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int birthMonth = 45;//simulated input
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+        System.out.println("What month were you born?");
+        if (in.hasNextInt());
+        {
+            birthMonth = in.nextInt();
+            if (birthMonth >=1 && birthMonth <=12)
+            {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+            else
+            {
+                System.out.println("You entered an incorrect month value " + birthMonth);
+            }
+        }
 
-        if (birthMonth >=1 && birthMonth <=12)
-        {
-            System.out.println("Your birth month is: " + birthMonth);
-        }
-        else
-        {
-            System.out.println("You entered an incorrect month value " + birthMonth);
-        }
+
     }
 }
